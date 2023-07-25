b0e1070fddfff56e8ab1062a7c18596907a28cae
physics-archive <defreshrants@gmail.com>
2023-07-24 21:15:17 -0400

    fixes

---
+++ MATLAB/atoms/ComputeEnergy.m
@@ -0,0 +1,6 @@
+Density_air = 1.2;
+
+Density = Density_air;
+
+Energy_Test=Energy(300,2,0.01);
+
---
+++ MATLAB/atoms/Energy.m
@@ -0,0 +1,9 @@
+function [Energy] = Energy(R,Density,time)
+%ENERGY Summary of this function goes here
+%   Detailed explanation goes here
+
+
+Energy = R.^5 .* Density .* time;
+
+end
+
